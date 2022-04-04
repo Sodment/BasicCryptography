b9ca6e342b5f8ea1898cfa37aadd4b39f853d5d8
Sodment <sodment@gmail.com>
2022-04-04 17:15:19 +0200

    Update lab4

--- lab4-bbs-rsa/lab4.py
+++ lab4-bbs-rsa/lab4.py
@@ -1,5 +1,5 @@
-from stest import stest
-from random import randint
+import stest
+from random import randint, getrandbits
 from time import time
 from math import gcd
 from collections import deque
@@ -7,38 +7,91 @@ from copy import copy
 
 path_to_save = "lab4-bbs-rsa/test_1mln_1.txt"
 p, q = 11, 23
-bits_to_generate = 10**6
+bits_to_generate = 1000000
 
 
-def bbs(p, q, k):  # Blum Blum Shub Random Bit generator. Input two large primes 3mod4 and the number of bits in the output
+def decompose(n):
+    exponentOfTwo = 0
 
-    t = time()				      # start the clock
-    rngs = deque()          # store the rngs for testing
+    while n % 2 == 0:
+        n = n//2
+        exponentOfTwo += 1
 
-    n = p*q
-    # phi is the number of elements in the multiplicative group of Z/nZ
-    phi = (p-1) * (q-1)
+    return exponentOfTwo, n
 
-    x_0 = (p-1)					    # x_0 is the seed
-    while (gcd(x_0, phi) != 1):  # this block of code chooses x_0 so that gcd(x_0,phi)=1
-        x_0 = randint(2, phi-1)
 
-    z = 0					            # initialize z (the bit component)
+def isWitness(possibleWitness, p, exponent, remainder):
+    possibleWitness = pow(possibleWitness, remainder, p)
 
-    for _ in range(k):      # generate sequence of k integers
-        x_0 = (x_0*x_0) % n
+    if possibleWitness == 1 or possibleWitness == p - 1:
+        return False
 
-        if (x_0 > n / 2):
-            x_0 = n - x_0
-        z = x_0 % 2
-        rngs.append(z)
+    for _ in range(exponent):
+        possibleWitness = pow(possibleWitness, 2, p)
 
-    t = time()-t
-    print('--------')
-    print(f'{k} bits generated with BBS')
-    print(f'Generating time: {t}')
+        if possibleWitness == p - 1:
+            return False
 
-    return rngs
+    return True
+
+
+def probablyPrime(p, accuracy=100):
+    if p == 2 or p == 3:
+        return True
+    if p < 2:
+        return False
+
+    exponent, remainder = decompose(p - 1)
+
+    for _ in range(accuracy):
+        possibleWitness = randint(2, p - 2)
+        if isWitness(possibleWitness, p, exponent, remainder):
+            return False
+
+    return True
+
+
+def goodPrime(p):
+    return p % 4 == 3 and probablyPrime(p, accuracy=100)
+
+
+def findGoodPrime(numBits=128):
+    candidate = 1
+    while not goodPrime(candidate):
+        candidate = getrandbits(numBits)
+    return candidate
+
+
+def makeModulus():
+    return findGoodPrime() * findGoodPrime()
+
+
+def parity(n):
+    return sum(int(x) for x in bin(n)[2:]) % 2
+
+
+class BlumBlumShub(object):
+    def __init__(self, seed=None):
+        self.modulus = makeModulus()
+        self.state = seed if seed is not None else randint(2, self.modulus - 1)
+        self.state = self.state % self.modulus
+
+    def seed(self, seed):
+        self.state = seed
+
+    def bitstream(self):
+        while True:
+            yield parity(self.state)
+            self.state = pow(self.state, 2, self.modulus)
+
+    def bits(self, n=20):
+        outputBits = ''
+        for bit in self.bitstream():
+            outputBits += str(bit)
+            if len(outputBits) == n:
+                break
+
+        return outputBits
 
 
 def rsa(p, q, k):
@@ -74,9 +127,10 @@ def rsa(p, q, k):
 
 if __name__ == '__main__':
     with open(path_to_save, "w") as f:
-        deq = bbs(p, q, bits_to_generate)
-        print(deq)
-        stest(copy(deq))
-        f.write("".join(str(i) for i in deq))
-
-    print("Succes!")
+        generator = BlumBlumShub()
+        res = generator.bits(bits_to_generate)
+        f.write(res)
+        stest.bits_test(res)
+        stest.series_test(res)
+        stest.long_series_test(res)
+        stest.poker_test(res)
--- lab4-bbs-rsa/stest.py
+++ lab4-bbs-rsa/stest.py
@@ -1,48 +1,151 @@
-from math import floor
+import string
+from scipy.special import comb
+from scipy.stats import chi2
+import numpy as np
+from collections import Counter
+import math
+import textwrap
+import pandas as pd
 
 
-def stest(rngs):
-
-    size = len(rngs)                # here rngs should be a deque type
-
-    freq = 0                        # here we count the number of 1s
-    switch = 0                      # here we count the number of bit switches
-
-    # poker test
-    # each entry correspond to the binary representation
-    poker = [0]*32
-    # for example, poker[2] count the number of bit sequences
-    # of the form 00010
-
-    switchtemp = 0                    # temp variable for switch
-
-    numof5sequence = floor(size/5)
-    for _ in range(numof5sequence):  # we consider disjoint 5 bit sequences
-
-        index = 0             # this will tell us what kind of bit sequence this is
-
-        for i in range(5):            # we consider disjoint 5 bit sequences
-            z = rngs.popleft()
-            freq += z                   # here we count 1s
-            switch += z ^ switchtemp       # here we count switches-"^" is xor
-            switchtemp = z
-
-            # here we convert the 5 bit sequence into
-            index += pow(2, 4-i)*z
-            # the index for the poker array
-        poker[index] += 1
+def bits_test(s: list):
+    ones = s.count('1')
+    if 9725 < ones < 10275:
+        print(f"BITS TEST: PASSED, Value of test {ones}")
+    else:
+        print(f"BITS TEST: FAILED, Value of test {ones}")
 
 
-# here we compute chi square of poker test
-    chi2 = 0
-    expected = float(numof5sequence)/32
-    for i in poker:
-        chi2 += pow(i-expected, 2)/expected
-# we test multiples of 5 bits so we set tsize to that
-    tsize = (size//5)*5
+def series_test_2(s: string):
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for i in range(1, 13):
+        series_1 = '0' + (i * '1') + '0'
+        series_0 = '1' + (i * '0') + '1'
+        s = s.replace(series_1, alpha[i])
+        s = s.replace(series_0, alpha[13+i])
 
-    print(f'Frequency of 1s: {float(freq)/tsize}')
-    print(f'Frequency of switches: {float(switch)/tsize}')
-    print(f'Chi Square of Poker test: {chi2}')
+    print(s)
+    print(s.count('b'))
 
-    return [float(freq)/tsize, float(switch)/tsize, chi2]
+
+def series_test(s: string):
+    result_ones = []
+    result_zeros = []
+    for i in range(1, 26):
+        result_ones.append(s.count(i * '1'))
+        result_zeros.append(s.count(i * '0'))
+    result_ones[6] = sum(result_ones[6:])
+    result_ones = result_ones[:6]
+    result_zeros[6] = sum(result_zeros[6:])
+    result_zeros = result_zeros[:6]
+
+    for i in range(6):
+        result_ones[i] = result_ones[i] - sum(result_ones[i+1:])
+        result_zeros[i] = result_zeros[i] - sum(result_zeros[i+1:])
+
+    intervals = [(2315, 2685), (1114, 1386), (527, 723),
+                 (240, 384), (103, 209), (103, 209)]
+    result = 1
+    for i in range(6):
+        if not(intervals[i][0] < result_ones[i] < intervals[i][1]):
+            result = 0
+    if result == 1:
+        print(
+            f"SERIES TEST: PASSED, Value of test 1:{result_ones}, 0:{result_zeros}")
+    else:
+        print(
+            f"SERIES TEST: PASSED, Value of test 1:{result_ones}, 0:{result_zeros}")
+
+
+def long_series_test(s: list):
+    ones = s.count(26*'1')
+    zeros = s.count(26*'0')
+
+    if zeros == 0 and ones == 0:
+        print(f"LONG SERIES TEST: PASSED, Value of test 1:{ones}, 0:{zeros}")
+    else:
+        print(f"LONG SERIES TEST: FAILED, Value of test 1:{ones}, 0:{zeros}")
+
+
+def poker_test(s: string):
+    splitted = textwrap.wrap(s, 4)
+    bits = {
+        '0000': 0,
+        '0001': 0,
+        '0010': 0,
+        '0011': 0,
+        '0100': 0,
+        '0101': 0,
+        '0110': 0,
+        '0111': 0,
+        '1000': 0,
+        '1001': 0,
+        '1010': 0,
+        '1011': 0,
+        '1100': 0,
+        '1101': 0,
+        '1110': 0,
+        '1111': 0,
+    }
+    for hex_number in splitted:
+        bits[hex_number] += 1
+
+    result = []
+    for val in bits:
+        x = bits[val]**2
+        result.append(x)
+    result = sum(result)
+    result = 16/5000 * result - 5000
+    if 2.16 < result < 46.17:
+        print(f"POKER TEST: PASSED, Value of test {result}")
+    else:
+        print(f"POKER TEST: FAILED, Value of test {result}")
+
+
+def poker_test_2(s, m):
+    X2theoretical = [3.84, 5.99, 7.81, 9.48, 11.07, 12.59, 14.06]
+    k = len(s)//m
+    l = list(np.arange(0, k))
+    s1 = s
+    for i in range(0, k):
+        while len(s1) > 0:
+            l[i] = s1[:m]
+            s1 = s1[m:]
+            break
+    n = l
+    for j, i in enumerate(l):
+        try:
+            n[j] = Counter(i)['1']
+        except:
+            n[j] = Counter(i)['0']
+
+    n.sort()
+    niDict = dict(Counter(n))
+    # we create a dummy dictionary with keys
+    k = [i for i in range(0, m+1)]
+    dummydict = dict(zip(k, [0]*len(k)))
+
+    def check_existance(i, collection: iter):
+        return i in collection
+    if dummydict.keys() == niDict.keys():
+        print('ok')
+    else:
+        for i in dummydict.keys():
+            if check_existance(i, niDict.keys()) == False:
+                niDict[i] = 0
+    b = []
+
+    for i in niDict.keys():
+
+        numerator = math.pow(niDict[i] - comb(m, i)*len(s)/((2**m)*m), 2)
+        denominator = comb(m, i)*len(s)/((2**m)*m)
+        S = numerator / denominator
+        b.append(S)
+
+    X2 = sum(b)
+    if X2 < chi2.isf(0.05, m-1):
+        print('POKER TEST: PASSED')
+    else:
+        print('POKER TEST:')
+    return X2
